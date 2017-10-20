b372de721b5b0ad268b4d88a326564f1140ddc0b
Pavel Sannikau <Pavel_Sannikau@epam.com>
2017-10-20 14:00:34 +0300

    Add project files.

---
+++ Crawler2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crawler2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            richTextBox1.Text = "";
+
+            Crawler crawler = new Crawler(Word.Text, int.Parse(Deep.Text));
+
+            // when DownloadingGroupSize >>> and TimeLimitToDownload <<< than crawler works faster but quality is worse (next example works faster)
+            // crawler.DownloadingGroupSize = 250;
+            // crawler.TimeLimitToDownload = 5;
+
+            var result = await crawler.StartAsync(Url.Text);
+
+            richTextBox1.Text += "Total: " + result.Count() + "\r\n";
+            foreach (var url in result) {
+                richTextBox1.Text += url + "\r\n";
+            }
+
+            button1.Enabled = true;
+        }
+    }
+}
